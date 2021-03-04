7eaecf59b3b6d2f779fa76c26f2ccfdda66f49b4
noahyzhang <noahyzhang@tencent.com>
2021-03-04 14:49:44 +0800

    first push

---
+++ cmake-build-debug/CMakeFiles/BookSystem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BookSystem"
+  "BookSystem.pdb"
+  "CMakeFiles/BookSystem.dir/book.cpp.o"
+  "CMakeFiles/BookSystem.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
